77f2763015c7267e3e7f1159fc012be0b7e100af
artyomtrus <trusartyom@yandex.by>
2022-10-08 16:45:32 +0300

    front and back 15

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
