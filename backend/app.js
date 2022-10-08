0c064fedc002c3499e3ff94e0ac1dd1226b6b0ce
artyomtrus <trusartyom@yandex.by>
2022-10-08 17:00:34 +0300

    front and back 15

--- backend/app.js
+++ backend/app.js
@@ -30,10 +30,18 @@ app.use(limiter);
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  next();
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
 });
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
