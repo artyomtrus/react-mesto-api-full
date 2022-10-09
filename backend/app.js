2065b8dabce119aa4c99d9fa7c999b7b86657915
artyomtrus <trusartyom@yandex.by>
2022-10-09 15:15:21 +0300

    front and back 15

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 // const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
@@ -14,17 +14,39 @@ const limiter = rateLimit({
   max: 100,
 });
 
-const allowedCors = [
-  'https://artyom.trus.nomoredomains.icu/',
-  'http://artyom.trus.nomoredomains.icu/',
-  'localhost:3000',
-];
+// const allowedCors = [
+//   'https://artyom.trus.nomoredomains.icu/',
+//   'http://artyom.trus.nomoredomains.icu/',
+//   'localhost:3000',
+// ];
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// app.use(cors());
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   console.log(origin);
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     return res.end();
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//   }
+//   return next();
+// });
+
+app.use(cors({
+  origin: 'https://artyom.trus.nomoredomains.icu/',
+  credentials: true,
+}));
 
 // app.use(helmet());
 app.use(limiter);
@@ -34,24 +56,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(requestLogger);
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  console.log(origin);
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    return res.end();
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-  }
-  return next();
-});
 
 app.use(router);
 
