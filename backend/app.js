ccd0704ce0c3bf9dceabc195aac340465062d8f2
artyomtrus <trusartyom@yandex.by>
2022-10-10 23:20:09 +0300

    front and back 15

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
     // res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
