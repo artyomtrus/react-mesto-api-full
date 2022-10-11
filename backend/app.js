fe9f5c1ebf39aa06347723afc518a7ac1dc5231c
artyomtrus <trusartyom@yandex.by>
2022-10-11 21:29:30 +0300

    front and back 15

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
-// const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
@@ -31,7 +29,6 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    // res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
@@ -41,12 +38,6 @@ app.use((req, res, next) => {
   return next();
 });
 
-// app.use(cors({
-//   origin: 'https://artyom.trus.nomoredomains.icu/',
-//   credentials: true,
-// }));
-//
-// // app.use(helmet());
 app.use(limiter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -54,6 +45,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(router);
 
