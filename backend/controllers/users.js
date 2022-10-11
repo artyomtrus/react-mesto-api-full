6751ce9413839952ed74806c29a526721aed0f57
artyomtrus <trusartyom@yandex.by>
2022-10-11 21:46:30 +0300

    front and back 15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,4 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -100,7 +101,7 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        'some-secret-key',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
       res.send({ token });
