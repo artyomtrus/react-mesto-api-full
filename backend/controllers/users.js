3bca5939afce1359f8fede624188e0ed2ac99420
artyomtrus <trusartyom@yandex.by>
2022-10-15 10:53:58 +0300

    front and back 15 исправления

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -104,7 +104,7 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-super',
         { expiresIn: '7d' },
       );
       res.send({ token });
